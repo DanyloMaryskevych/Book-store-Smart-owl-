fa3106c45de67d7d14082d808795733b1c0d3306
Danylo Maryskevych <danylo.maryskevych@gmail.com>
2019-03-03 23:52:46 +0200

    Add check mark after button click

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 $('button').click(function() {
     $(this)
+        .slideUp(500)
         .closest('div')
+        .css('background-color', 'rgba(0, 255, 0, 0.3)')
         .find('a > img')
         .clone()
         .addClass('zoom')
@@ -9,4 +11,8 @@ $('button').click(function() {
     setTimeout(function() {
         $('.zoom').remove();
     }, 1500);
+})
+
+$('button').click(function() {
+    $(this).next().slideDown(500)
 })
